f8803062e1fb80ec6286da4211ec254c8aaa890e
mojtaba zarei <zarei.bs@gmail.com>
2016-03-09 18:18:56 +0330

    #comment init application table

---
+++ app/userApplications/applicationTable/applicationTableController.js
@@ -0,0 +1,9 @@
+/**
+ * Created by mojtaba on 3/9/16.
+ */
+/**
+ * @ngdoc controller
+ * @name adminsPanel.controller:applicationTableController
+ * @description
+ * it can do CRUD on user's applications by using smartTable
+ */
---
+++ app/userApplications/applicationTable/applicationTableDirective.js
@@ -0,0 +1,24 @@
+/**
+ * Created by mojtaba on 3/9/16.
+ */
+
+/**
+ * @ngdoc directive
+ * @name global.directive:applicationTable
+ * @scope
+ * @restrict E
+ *
+ * @description
+ * it is a component to manage applications
+ *
+ * @param {object}  field   A field object
+ *
+ */
+module.directive('applicationTable', function() {
+    return {
+        scope: true,
+        controller: 'applicationTableController',
+        controllerAs: 'appCtrl',
+        templateUrl: 'applicationTable.html'
+    };
+});
