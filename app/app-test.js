924626de3e50548eb653852d46a951d54fc2c140
mojtaba zarei <zarei.bs@gmail.com>
2016-03-09 13:31:23 +0330

    rewrite app test

--- app/app-test.js
+++ app/app-test.js
@@ -8,3 +8,27 @@ describe('first Test', function() {
         expect($('body').length).toEqual(1);
     });
 });
+describe('test app Controller', function() {
+    beforeEach(module('app'));
+    var $controller;
+    beforeEach(inject(function(_$controller_){
+        // The injector unwraps the underscores (_) from around the parameter names when matching
+        $controller = _$controller_;
+    }));
+
+    var $scope = {$watch:function(){}};
+
+
+    it('methods are defined well', function () {
+        var controller = $controller('AppCtrl', { $scope: $scope });
+        expect(typeof $scope.isIE()).toEqual('boolean');
+        expect(typeof $scope.isSmart()).toEqual('boolean');
+
+        expect(typeof $scope.setTheme).toEqual('function');
+        expect(typeof $scope.goBack).toEqual('function');
+    });
+    it('name has to inherit from parent scope', function () {
+        var controller = $controller('AppCtrl', { $scope: $scope });
+        expect($scope.app.name.length>0).toEqual(true);
+    });
+});
