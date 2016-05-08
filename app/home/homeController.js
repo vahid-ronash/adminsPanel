94bb1f1a73b2ec1ff80d3b33ec67585491084695
fingerpich <mojtaba.zarei@ronash.co>
2016-05-08 15:06:24 +0430

    #time 10m add home page

---
+++ app/home/homeController.js
@@ -0,0 +1,19 @@
+/**
+ * Created by mojtaba on 5/8/16.
+ */
+/*global angular */
+/**
+ * @ngdoc controller
+ * @name app.controller.homeController
+ * @description
+ * control sign in page and make user able to login
+ */
+((function() {
+    'use strict';
+    angular
+        .module('app')
+        .controller('homeController', ['$scope', 'AuthService', '$location', '$timeout', function ($scope, $AuthService, $location, $timeout) {
+            var thisController = this;
+
+        }]);
+})());
